a69bafc9c62f163c05485350389e1419a8ddf86e
niklasnikanti <niklas.nikanti@gmail.com>
2020-01-13 20:06:23 +0200

    added path

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,16 @@ const app = express();
 const port = process.env.PORT || 3000;
 const lunchParser = require("./lunch_parser");
 const env = app.get("env");
+const path = require("path");
 console.log("env", env);
+
+
+const frontend_path = path.join("..", "/frontend", "/www");
+console.log("front end path", frontend_path); // debug
 
 // Serve the page.
 // if (env === "development") app.use(express.static("../frontend"));
-/*else*/ app.use(express.static("../frontend/www"));
+/*else*/ app.use(express.static(frontend_path));
 
 // Listen for the lunch list requests.
 app.get("/lunches", async (req, res) => {
