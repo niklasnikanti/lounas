e1d6f1d08d7531a939af75831c5406c6ea45a8be
niklasnikanti <niklas.nikanti@gmail.com>
2020-01-15 20:37:23 +0200

    Installed ws

--- backend/index.js
+++ backend/index.js
@@ -4,14 +4,32 @@ const port = process.env.PORT || 3000;
 const lunchParser = require("./lunch_parser");
 const env = app.get("env");
 const path = require("path");
+const WebSocket = require("ws");
 console.log("env", env);
 
 // Security best practices.
 app.disable("x-powered-by");
 
-
-const frontend_path = path.join("..", "/frontend", "/www");
+const frontend_path = path.join("..", "frontend", "www");
 console.log("front end path", frontend_path); // debug
+
+// Open WebSocket server.
+const ws = new WebSocket.Server({ port: 8080 });
+
+// TODO: Test client - server connection.
+ws.on("open", function open() {
+	console.log("WebSocket server is open!");
+	ws.send("test message");
+});
+
+ws.on("message", function incoming(data) {
+	console.log("data", data);
+});
+
+ws.on("error", e => {
+	console.error("WebSocket error:", e);
+});
+
 
 // Serve the page.
 // if (env === "development") app.use(express.static("../frontend"));
